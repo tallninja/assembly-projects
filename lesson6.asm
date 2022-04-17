be827664e80a47f55db2ff51097c6e6ff1088267
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-17 11:39:36 +0300

    lesson6

---
+++ lesson6.asm
@@ -0,0 +1,26 @@
+; Lesson 6: In the previous lesson msg2 was being printed twice
+; this is because we are not terminating our strings
+; Compile: nasm -f elf lesson6.asm
+; Link: ld -m elf_i386 lesson6.o -o lesson6
+; Run: ./lesson6
+
+%include	'functions.asm'
+
+SECTION .data
+
+msg1	db	'Hello World One !', 0Ah, 0h
+msg2	db	'Hello World Two !', 0Ah, 0h
+
+SECTION .text
+
+global _start
+
+_start:
+
+	mov	eax, msg1
+	call	printstr
+
+	mov	eax, msg2
+	call	printstr
+
+	call	quit
